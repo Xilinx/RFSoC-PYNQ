9aaef18436273f2d21b17e81075229a8bedb7ac4
Graham Schelle <schelleg@users.noreply.github.com>
2022-10-02 20:36:29 -0600

    hotfix to constraints path

--- boards/ZCU208/base/build_bitstream.tcl
+++ boards/ZCU208/base/build_bitstream.tcl
@@ -12,7 +12,7 @@ open_bd_design ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name
 make_wrapper -files [get_files ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/${design_name}.bd] -top
 add_files -norecurse ./${overlay_name}/${overlay_name}.srcs/sources_1/bd/${design_name}/hdl/${design_name}_wrapper.v
 set_property top ${design_name}_wrapper [current_fileset]
-import_files -fileset constrs_1 -norecurse ./constraints/${overlay_name}.xdc
+import_files -fileset constrs_1 -norecurse ./vivado/constraints/${overlay_name}.xdc
 update_compile_order -fileset sources_1
 
 # set platform properties
