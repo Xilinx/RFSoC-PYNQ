e2204ded95d1d9de739a942c0e4c4b355c9f311f
josh goldsmith <josh.goldsmith@amd.com>
2026-08-07 10:14:01 +0100

    updated copyrights

--- boards/ZCU111/packages/smbus2/qemu.sh
+++ boards/ZCU111/packages/smbus2/qemu.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 # Copyright (C) 2022 Xilinx, Inc
+# Copyright (C) 2022-2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
 source /usr/local/share/pynq-venv/bin/activate
