9be456d62d231746d22323283d9f65df219a55b2
Josh Goldsmith <28935218+jogomojo@users.noreply.github.com>
2026-08-07 12:11:43 +0100

    add EDF 2025.2 board support for RFSoC4x2 (#65)
    * boards/RFSoC4x2: add PS-only Vivado BSP design for custom-SDT boot flow
    
    * boards/RFSoC4x2: add EDF 2025.2 board support
    
    * boards/RFSoC4x2/tics: detect the venv's Python version instead of hardcoding 3.10
    
    * boards/RFSoC4x2: drop dead xrfclk-patch package
    
    * boards/RFSoC4x2: add on-target self-test package
    
    * updated copyrights

--- boards/RFSoC4x2/packages/selftest/pre.sh
+++ boards/RFSoC4x2/packages/selftest/pre.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-# Copyright (C) 2021 Xilinx, Inc
+# Copyright (C) 2021-2022 Xilinx, Inc
+# Copyright (C) 2022-2026 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: BSD-3-Clause
 
 target=$1
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# the current boot.py will overwrite all existing boot.py
-sudo cp -rf $script_dir/boot.py $target/boot/boot.py
+sudo install -D -m 0755 "$script_dir/pynq-selftest" "$target/usr/local/bin/pynq-selftest"
