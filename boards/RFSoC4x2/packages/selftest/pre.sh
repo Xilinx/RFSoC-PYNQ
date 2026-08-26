91abcf0eaf656a51b870815102174f7c96aeb360
Josh Goldsmith <28935218+jogomojo@users.noreply.github.com>
2026-08-26 14:18:16 +0100

    Migrate RFSoC board selftests to JSON manifests (#71)

--- boards/RFSoC4x2/packages/selftest/pre.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# Copyright (C) 2021-2022 Xilinx, Inc
-# Copyright (C) 2022-2026 Advanced Micro Devices, Inc.
-# SPDX-License-Identifier: BSD-3-Clause
-
-target=$1
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-sudo install -D -m 0755 "$script_dir/pynq-selftest" "$target/usr/local/bin/pynq-selftest"
